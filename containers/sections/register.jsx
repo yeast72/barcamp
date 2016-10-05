a42629192299219ed5cde5d167623c31524a7c19
Wisarut Boonnumma <Wisarut Boonnumma>
2016-10-06 00:46:16 +0700

    fix margin

--- containers/sections/register.jsx
+++ containers/sections/register.jsx
@@ -1,34 +1,37 @@
 class Section extends React.Component {
   render() {
     return (
-      <body id="register-bg">
-        <div className="section flex flex-center" id="register-section">
-          <article className = "uk-article" id = "register-text">
-            <h1 class="uk-article-title" >WHAT IS <span id="register-B">B</span>
-            <span id ="register-A1">A</span>
-            <span id ="register-R">R</span>
-            <span id ="register-C">C</span>
-            <span id ="register-A2">A</span>
-            <span id ="register-M">M</span>
-            <span id ="register-P">P</span>?</h1>
-            <p className="uk-article-meta">Barcamp is a paricipant-generated conference primarily focused on exchanging knowledge,experience and stories. The participants will</p>
-            <p className="uk-article-meta">have a chance to come into contact with new ideas as well as propose some of their own.</p>
-            <p className="uk-article-meta">บาร์แคมป์เป็นงานชุมนุมรวมกลุ่มของผู้ที่สนใจแบ่งปันความรู้ ประสบการณ์ หรือเรื่องราวต่างๆให้ผู้ร่วมงานคนอื่นๆ ได้ฟังกัน งานนี้ไม่มีการกำหนดหัวข้อเอาไว้ล่วงหน้า</p>
-            <p className="uk-article-meta">ผู้ร่วมงานทุกคนจะเป็นผู้กำหนดหัวข้อในงาน</p>
-            <h1 class="uk-article-title">WHO IS PARTICIPATING?</h1>
-            <p className="uk-article-meta">Anybody!</p>
-            <p className="uk-article-meta">คุณไงละ! งานของเราจะขาดคุณผู้ซึ่งเป็นผู่แบ่งปันเรื่องราวดีๆไปไมได้</p>
-            <h1 class="uk-article-title">WHY SHOULD WE GO TO BARCAMP?</h1>
-            <p className="uk-article-meta">Along with me no entrance fee, the participants will be given the privilege to propose their ideas they want to talk about
-            once they have registered</p>
-            <p className="uk-article-meta">Given enough votes from people who are interested,the staff will re-schedule a session where anybody is welcome to join to
-            exhange ideas</p>
-            <p className="uk-article-meta">about the topic.</p>
-            <p className="uk-article-meta">เราไม่จำกัดหัวข้อในการนำเสนอ ถ้าคุณมีเรื่องราวที่สนใจอยากจะพูดคุย คุณสามารถเสนอหัวข้อที่ต้องการได้เลย แต่หากคุณยังคิดไม่ออก คุณก็สามารถเลือกหัวข้อที่คนอื่นเสนอและเข้าไปร่วมพูดคุยได้</p>
-            <p className="uk-article-meta">หัวข้อในงานของเรามีความหลากหลายแตกต่างกันไป หัวข้อที่ถูกเลือกอาจจะเกี่ยวกับเทคโนโลยี ทริปท่องเที่ยว สอนธรุกิจ รีวิวเกมส์ เอาตัวรอดในดินแดนซอมบี้ และอื่นๆอีกมากมาย</p>
+        <body id="register-bg">
+        <div class="uk-container uk-container-center">
+          <ul class="uk-grid uk-width-midium-1-1"id="register-section" data-uk-grid-margi>
+            <article className = "uk-article"id = "register-text">
+              <h1 class="uk-article-title ">WHAT IS <span id="register-B">B</span>
+              <span id ="register-A1">A</span>
+              <span id ="register-R">R</span>
+              <span id ="register-C">C</span>
+              <span id ="register-A2">A</span>
+              <span id ="register-M">M</span>
+              <span id ="register-P">P</span>?</h1>
+              <p className="uk-article-meta ">Barcamp is a paricipant-generated conference primarily focused on exchanging knowledge,experience and stories. The participants will</p>
+              <p className="uk-article-meta">have a chance to come into contact with new ideas as well as propose some of their own.</p>
+              <p className="uk-article-meta">บาร์แคมป์เป็นงานชุมนุมรวมกลุ่มของผู้ที่สนใจแบ่งปันความรู้ ประสบการณ์ หรือเรื่องราวต่างๆให้ผู้ร่วมงานคนอื่นๆ ได้ฟังกัน งานนี้ไม่มีการกำหนดหัวข้อเอาไว้ล่วงหน้า</p>
+              <p className="uk-article-meta">ผู้ร่วมงานทุกคนจะเป็นผู้กำหนดหัวข้อในงาน</p>
+              <h1 class="uk-article-title">WHO IS PARTICIPATING?</h1>
+              <p className="uk-article-meta">Anybody!</p>
+              <p className="uk-article-meta">คุณไงละ! งานของเราจะขาดคุณผู้ซึ่งเป็นผู่แบ่งปันเรื่องราวดีๆไปไมได้</p>
+              <h1 class="uk-article-title">WHY SHOULD WE GO TO BARCAMP?</h1>
+              <p className="uk-article-meta">Along with me no entrance fee, the participants will be given the privilege to propose their ideas they want to talk about
+              once they have registered</p>
+              <p className="uk-article-meta">Given enough votes from people who are interested,the staff will re-schedule a session where anybody is welcome to join to
+              exhange ideas</p>
+              <p className="uk-article-meta">about the topic.</p>
+              <p className="uk-article-meta"class="hidden-phone">เราไม่จำกัดหัวข้อในการนำเสนอ ถ้าคุณมีเรื่องราวที่สนใจอยากจะพูดคุย คุณสามารถเสนอหัวข้อที่ต้องการได้เลย แต่หากคุณยังคิดไม่ออก คุณก็สามารถเลือกหัวข้อที่คนอื่นเสนอและเข้าไปร่วมพูดคุยได้</p>
+              <p className="uk-article-meta"class="hidden-phone">หัวข้อในงานของเรามีความหลากหลายแตกต่างกันไป หัวข้อที่ถูกเลือกอาจจะเกี่ยวกับเทคโนโลยี ทริปท่องเที่ยว สอนธรุกิจ รีวิวเกมส์ เอาตัวรอดในดินแดนซอมบี้ และอื่นๆอีกมากมาย</p>
             </article>
-            </div>
-            </body>
+            </ul>
+          </div>
+        </body>
+
     );
   }
 }
